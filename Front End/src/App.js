c1b7fdeb236878b0853b811353eba12ffb9ecd25
Renan Garcia <renangarciasoz@hotmail.com>
2018-03-24 15:14:04 -0300

    Início do protótio em react

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import SimpleLineIcon from 'react-simple-line-icons';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div className="header-bar"></div>
+        <div className="menu-left">
+          <div className="logo-wrapper">
+            <div className="logo"></div>
+          </div>
+          <div className="menu-items">
+            <a href="#" className="item">
+              <div className="rectangle"></div>
+              <div className="item-label">
+               icone
+              </div>
+            </a>
+
+            <a href="#" className="item">
+              <div className="rectangle"></div>
+              <div className="item-label">
+               icone
+              </div>
+            </a>
+
+            <a href="#" className="item">
+              <div className="rectangle"></div>
+              <div className="item-label">
+               icone
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="main-content"></div>
       </div>
     );
   }
